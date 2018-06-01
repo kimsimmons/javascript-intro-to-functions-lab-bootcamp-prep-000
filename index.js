d58c9af97552ea60fb295ef385a66298b5321410
Kimberly Simmons <kimberlysimmonsr@gmail.com>
2018-06-01 22:48:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,3 +25,11 @@ function sayHiToGrandma(string){
       return 'I love you, too.'
     }
 }
+
+function sayHiToGrandma(string){
+if (string.toUpperCase() === string){
+      return 'YES INDEED!'
+    }
+    if (string === 'I love you, Grandma.'){
+      return 'I love you, too.'
+    }
